f2b9a20212a487e679036a52d7871c9d3d90cb5c
a-yurev_87 <a-yurev_87@mail.ru>
2022-08-19 07:40:17 +0300

    Создал проект task1

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите квадрат число: ");
+int square = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = num * num;
+if (square == result) 
+{
+    Console.WriteLine($"a = {square}, b = {num} -> да");
+}
+else
+{
+    Console.WriteLine($"a = {square}, b = {num} -> нет");
+}
+
