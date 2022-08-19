2f012e97e493ba20dc7e3a159f772fe6513f6eaa
a-yurev_87 <a-yurev_87@mail.ru>
2022-08-19 07:45:34 +0300

    Создал проекта task1_week

---
+++ task1_week/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число неделя: ");
+int week = Convert.ToInt32(Console.ReadLine());
+if (week == 1) System.Console.WriteLine("Понедельник");
+else if(week == 2) System.Console.WriteLine("Вторник");
+else if(week == 3) System.Console.WriteLine("Среда");
+else if (week == 4) System.Console.WriteLine("Четверг");
+else if (week == 5) System.Console. WriteLine("Пятница");
+else if (week == 6) System.Console. WriteLine("Суббота");
+else if (week == 7) System.Console.WriteLine("Воскресенье");
+else System.Console.WriteLine("Не существовует,а только диапазона с 1 до 7 числа");
+
