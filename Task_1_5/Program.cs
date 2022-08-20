e9574d7492790c8de5b97b9f719c2be2efa47dd2
a-yurev_87 <a-yurev_87@mail.ru>
2022-08-20 13:35:10 +0300

    дополнительно  задачу Task_1_5

--- Task_1_5/Program.cs
+++ Task_1_5/Program.cs
@@ -1,9 +1,19 @@
 ﻿Console.Write("Введите целое число: ");
 int num = Convert.ToInt32(System.Console.ReadLine());
-
+int num1 = num;
 Console.Write("Вывод чисел в промежутку от -N до N: " + -num);
 
 for (int i = -num + 1 ; i <= num; i++ )
 {
     Console.Write(", " + i);
+}
+Console.WriteLine("");
+ int num2 = -num1;
+ Console.Write(-num1);
+  
+while (num2 < num1)
+{
+    num2++;
+    Console.Write($", {num2}");
+
 }
