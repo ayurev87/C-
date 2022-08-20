6f5614296073baacd7de6a6061e9ae20ca098a62
a-yurev_87 <a-yurev_87@mail.ru>
2022-08-20 13:05:34 +0300

    Переименовали папки

---
+++ Task_1_5/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(System.Console.ReadLine());
+
+Console.Write("Вывод чисел в промежутку от -N до N: " + -num);
+
+for (int i = -num + 1 ; i <= num; i++ )
+{
+    Console.Write(", " + i);
+}
