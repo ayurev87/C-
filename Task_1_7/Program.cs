608d33094ed9d2b1972c576416709a43fd638fe6
a-yurev_87 <a-yurev_87@mail.ru>
2022-08-20 13:23:27 +0300

    дополнительно  задачу Task_1_7

--- Task_1_7/Program.cs
+++ Task_1_7/Program.cs
@@ -1,3 +1,7 @@
-﻿Console.Write("Введите трех числа: ");
+﻿Console.Write("Введите числа: ");
 int num = Convert.ToInt32(System.Console.ReadLine());
-Console.WriteLine($"Вывод последние числа из них: {num % 10}");
+if ( num < 0)
+{
+    Console.WriteLine($"Вывод последние числа из них: {-num % 10}");
+}
+else Console.WriteLine($"Вывод последние числа из них: {num % 10}");
