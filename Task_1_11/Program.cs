6138140412458be2e72a06ace5fd1b17925632c8
a-yurev_87 <a-yurev_87@mail.ru>
2022-08-27 09:05:56 +0300

    Add task_1_11

---
+++ Task_1_11/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишете программу, которая выводит случайнре трёхзначенное число и удаляет вторую цифру этого числа.
+
+int number = new Random().Next(100, 1000);
+int FirstDigit = number / 100;
+int SecondDigit = number % 10;
+
+ Console.WriteLine("Трёхзначенное число: " + number);
+// Console.WriteLine($"Убрать вторую цифру и получилось: {FirstDigit}{SecondDigit}");
+ int enter = FirstDigit *10 + SecondDigit;
+
+Console.WriteLine($"Убрать вторую цифру и получилось: {enter}");
