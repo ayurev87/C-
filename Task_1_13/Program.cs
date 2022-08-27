63d4905136a5b229a63a4c6a5cab599db23752c6
a-yurev_87 <a-yurev_87@mail.ru>
2022-08-28 01:04:55 +0300

    add task_1_13

---
+++ Task_1_13/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. Решить без использования строк.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int num = new Random().Next(10,999999);
+Console.WriteLine("Случайно числа выпадает: " + num);
+// int cont = 100;
+// if ( num/cont == 0) Console.WriteLine("Третьй цифры нет");
+// else 
+// {
+//     while ( num/cont > 0) 
+//     {
+//         cont=cont*10;
+//     }
+//     int num1 = num/(cont/1000);
+//     int result =num1 - (num1/10*10);
+//     Console.WriteLine("Третий цифр: " + result);
+// }
+string Third (int numer)
+{
+    int cont = 100;
+    if ( numer/cont == 0) 
+        return ("Третьй цифры нет");
+    else 
+    {
+        while ( num/cont > 0) 
+        {
+            cont=cont*10;
+        }
+        int num1 = num/(cont/1000);
+        int result =num1 - (num1/10*10);
+        return ("Третий цифр: " + result);
+    }
+}
+string result = Third(num);
+Console.WriteLine(result);
