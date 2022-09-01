80b21ae6dcc79e62d76ad7721e3689b28332cef0
ayurev87 <a-yurev_87@mail.ru>
2022-09-01 17:56:40 +0300

    add task_1_23

---
+++ Task_1_23/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите число (N): ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n < 1) Console.WriteLine("Введены некорректное число");
+else
+{
+    for(int i = 1; i <= n; i++)
+    {
+        Console.WriteLine(i + " | " + Math.Pow(i,3));
+    }
+}
