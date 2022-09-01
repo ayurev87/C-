51ca694cd64352b0fe175d01a2be1d2237fd699e
a-yurev_87 <a-yurev_87@mail.ru>
2022-09-01 08:23:06 +0300

    add task_1_22

---
+++ Task_1_22/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 22. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N
+try
+{
+    Console.Write("Введите число N: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    if (n < 1) Console.WriteLine("Введены некорректное значение");
+    // else
+    // {
+    //     int count = 0;
+    //     Console.Write( Math.Pow(1, 2));
+    //     while (count < n)
+    //     {
+    //         count++;
+    //         Console.Write(", " + Math.Pow(count+1, 2));
+    //     }
+    // }
+    else
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            Console.WriteLine($" {i}\t{Math.Pow(i, 2)}");
+        }
+    }
+}
+catch
+{
+    Console.WriteLine("Введены некорректное значение");
+}
