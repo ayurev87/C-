7ede346e3c18cf29803e73fd30724e3a5a5f9456
ayurev87 <a-yurev_87@mail.ru>
2022-09-10 19:18:23 +0300

    исправить task_1_39

--- Task_1_42/Program.cs
+++ Task_1_42/Program.cs
@@ -2,3 +2,31 @@
 // 45 -> 101101
 // 3 -> 11
 // 2 -> 10
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+//краткое проще 1 вариант.
+// void Main(int val)
+// 	{
+// 		string binary = Convert.ToString(val, 2);
+
+// 		Console.WriteLine(binary);
+// 	}
+// Main(number);
+
+
+// 2  вариант
+while (number > 0)
+{
+    int num1 = number % 2;
+    Console.WriteLine(num1);
+    number /=2;
+}
+
+
+
+
+
+
+
