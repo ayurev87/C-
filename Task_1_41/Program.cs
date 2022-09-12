d2162cb84794a4e38206f6093a60b1cd8fa11dc5
ayurev87 <a-yurev_87@mail.ru>
2022-09-12 19:41:03 +0300

    add task_1_41

---
+++ Task_1_41/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+
+Console.Write("Введите количество массив: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] CreateArrRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ", ");
+        else Console.Write(arr[i] + " ]");
+    }
+}
+
+void PosSizeArray(int[] arr)
+{
+    int pos = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) pos++;
+    }
+    Console.WriteLine($" -> {pos} положительные чисел");
+}
+
+int[] array1 = CreateArrRndInt(size, -300, 300);
+PrintArray(array1);
+PosSizeArray(array1);
