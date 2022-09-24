dbb326e4377cdf1d2e942014e8ddef4d5d70c125
a-yurev_87 <a-yurev_87@mail.ru>
2022-09-24 20:45:48 +0300

    add task_1_67

---
+++ Task_1_67/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 67. Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.Write("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumDigitals(int num)
+{   
+    int sum = num%10;
+    if(num == 0) return sum;
+    return SumDigitals(num/10) + sum;
+    
+}
+
+Console.WriteLine($"{number} -> {SumDigitals(number)}");
